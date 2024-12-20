82ab80478b646e41f7f97ace83d3e09e4afe539e
Tristan Kermorvant <tristan.kermorvant@gmail.com>
2024-12-20 16:49:17 +0100

    🚧 WIP: Worked on ui

--- src/App.tsx
+++ src/App.tsx
@@ -24,24 +24,38 @@ function App() {
   }, [word]);
 
   return (
-    <>
-      <h1>Der Die Das</h1>
-      <Input
-        value={word}
-        onChange={(e) => setWord(e.target.value)}
-        placeholder="Enter a German word"
-      />
-      {suggestions.length > 0 && (
-        <ul>
-          {suggestions.map((suggestion, index) => (
-            <li key={index}>
-              {suggestion} -{" "}
-              {genreMap[getGenderGermanWord(suggestion)!] || "No genre found"}
-            </li>
-          ))}
-        </ul>
-      )}
-    </>
+    <div className="flex flex-col items-center min-h-screen bg-gray-100">
+      <div className="container flex flex-col max-w-screen-xl">
+        <div
+          className="flex min-h-[480px] bg-cover bg-center bg-no-repeat text-white rounded-2xl p-4 mt-8"
+          style={{
+            backgroundImage: `url("/germany.png")`,
+          }}
+        >
+          <h1 className="text-3xl font-bold mb-6">Der Die Das</h1>
+        </div>
+
+        <Input
+          value={word}
+          onChange={(e) => setWord(e.target.value)}
+          placeholder="Enter a German word"
+          className="w-full p-4 text-lg border border-gray-300 mb-4"
+        />
+        {suggestions.length > 0 && (
+          <ul className="w-full max-w-md bg-white shadow-md rounded">
+            {suggestions.map((suggestion, index) => (
+              <li
+                key={index}
+                className="p-4 border-b border-gray-200 last:border-b-0"
+              >
+                {suggestion} -{" "}
+                {genreMap[getGenderGermanWord(suggestion)!] || "No genre found"}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
   );
 }
 
